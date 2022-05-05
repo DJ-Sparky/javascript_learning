12e915d2c71a7d6990b3147556a694abdfc92c54
DJ-Sparky <Sparky@ravemail.com>
2022-05-05 18:47:31 +0100

    adding Dates for Copyrights

--- 001_Hello_World/hello.js
+++ 001_Hello_World/hello.js
@@ -20,3 +20,9 @@ console.log(time);
 
 const date = new Date();
 console.log(date);
+
+let year = date.getFullYear();
+
+document.getElementById('copyright').innerHTML = '© Sparky' + ' ' + year;
+
+console.log(year);
